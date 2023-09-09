98047eaaee44489ea9f43ac9eff898dc6c46b97a
ClownNero <hk5426@nate.com>
2023-09-10 04:20:39 +0900

    9/10 last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer(props) {
     <>
       <footer className="p-4 text-2xl shadow-up">
         <ul className="w-full flex-col text-center text-sm">
-          <li className="mr-2">
+          <li>
             Copyright © {new Date().getFullYear()} YourCode-X
           </li>
-          <li className="mr-2">
+          <li>
             (49315) 부산 사하구 낙동대로 550번길 37 (하단동)
           </li>
-          <li className="mr-2">
+          <li>
             Email: YourCode-X@donga.ac.kr | Phone: 010-3351-5426
           </li>
         </ul>
