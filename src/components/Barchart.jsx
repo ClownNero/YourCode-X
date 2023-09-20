c9042381879f5b11672dab456ba138fbc049e43d
ClownNero <hk5426@nate.com>
2023-09-21 05:56:54 +0900

    9/20 last commit

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -3,9 +3,24 @@ import ReactEcharts from 'echarts-for-react';
 
 export default function Barchart({data}) {
   const [options, setOptions] = useState({
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
     xAxis: {
       type: 'category',
       axisLabel: { interval: 0, rotate: 30 },
+      axisTick: {
+        alignWithLabel: true
+      },
       data: data.map(item => item.category)
     },
     yAxis: {
