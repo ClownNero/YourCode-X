74cf3c411713c0ab016388b8f27d3b15e9f78f27
ClownNero <ClownNero@gmail.com>
2023-09-22 14:19:57 +0900

    Modify Barchart

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -1,44 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import ReactEcharts from 'echarts-for-react';
+import React, { useState, useEffect } from "react";
+import ReactEcharts from "echarts-for-react";
 
-export default function Barchart({data}) {
+export default function Barchart({ data }) {
   const [options, setOptions] = useState({
     tooltip: {
-      trigger: 'axis',
+      trigger: "axis",
       axisPointer: {
-        type: 'shadow'
-      }
+        type: "shadow",
+      },
     },
     grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
     },
     xAxis: {
-      type: 'category',
-      axisLabel: { interval: 0, rotate: 30 },
+      type: "category",
+      axisLabel: { show: false },
       axisTick: {
-        alignWithLabel: true
+        alignWithLabel: true,
       },
-      data: data.map(item => item.category)
+
+      data: data.map((item) => item.category),
     },
     yAxis: {
-      type: 'value'
+      type: "value",
+      axisLabel: { show: false },
+      splitLine: { show: false },
     },
     series: [
       {
-        data: data.map(item => item.risk),
-        type: 'bar'
-      }
-    ]
+        data: data.map((item, index) => ({
+          value: item.risk,
+          itemStyle: {
+            color:
+              index % 3 === 0
+                ? "#2D5FFF"
+                : index % 3 === 1
+                ? "#1E82E8"
+                : "#21CAFF",
+            borderRadius: [50, 50, 0, 0], //막대 차트 윗부분 둥글게 만들기
+          },
+        })),
+        type: "bar",
+      },
+    ],
   });
   return (
     <>
-      <ReactEcharts
-        option = {options}
-      />
+      <ReactEcharts option={options} />
     </>
   );
 }
-
