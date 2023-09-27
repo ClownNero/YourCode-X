32c555fd629084cbe4b12bbc7bad7a72c74e4224
ClownNero <ClownNero@gmail.com>
2023-09-27 23:54:20 +0900

    Nothing

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-function Modal() {
+function Modal({ data }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
@@ -13,31 +13,57 @@ function Modal() {
 
   return (
     <div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={openModal}
+      >
         Open Modal
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50" onClick={closeModal}>
-          <div className="bg-white rounded-lg p-12 w-4/5 h-5/6 overflow-auto" onClick={(e) => e.stopPropagation()}>
+        <div
+          className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
+          onClick={closeModal}
+        >
+          <div
+            className="bg-white rounded-lg p-12 w-4/5 h-5/6 overflow-auto"
+            onClick={(e) => e.stopPropagation()}
+          >
             <div className="flex">
-              <div className='w-full pr-4 border-r overflow-y-auto'>
+              <div className="w-full pr-4 border-r overflow-y-auto">
                 <h2>chatGPT 검색 부분</h2>
                 {/* ChatGPT data */}
-                </div>
+              </div>
               <div className="flex flex-col p-4 w-full overflow-y-auto">
-                <h2 className='text-center text-3xl font-bold'>코드로 자세히 알아보기</h2>
-                <p className='text-center'>Learn more with code</p>
-                <div className='border-b pb-4'>
+                <h2 className="text-center text-3xl font-bold">
+                  코드로 자세히 알아보기
+                </h2>
+                <p className="text-center">Learn more with code</p>
+                <div className="border-b pb-4">
                   <h3>FeedBack</h3>
                   {/* FeedBack data 부분*/}
-                  <p>SQL Injection은 악의적인 사용자가 입력 데이터에 악의적인 SQL 코드를 삽입하여 데이터베이스에 대한 공격을 시도하는 보안 취약점입니다. 이를 방지하기 위해서는 입력 데이터를 안전하게 처리해야 합니다. 대부분의 프로그래밍 언어와 데이터베이스 시스템은 파라미터화된 쿼리를 사용하여 SQL Injection을 방지할 수 있습니다.</p>
+                  <p>
+                    SQL Injection은 악의적인 사용자가 입력 데이터에 악의적인 SQL
+                    코드를 삽입하여 데이터베이스에 대한 공격을 시도하는 보안
+                    취약점입니다. 이를 방지하기 위해서는 입력 데이터를 안전하게
+                    처리해야 합니다. 대부분의 프로그래밍 언어와 데이터베이스
+                    시스템은 파라미터화된 쿼리를 사용하여 SQL Injection을 방지할
+                    수 있습니다.
+                  </p>
                 </div>
-                <div className='pt-4'>
+                <div className="pt-4">
                   <h3>Comment</h3>
                   {/* Comment data 부분*/}
-                  <p>이 코드에서는 ?를 사용하여 입력 값을 파라미터화된 쿼리에 전달합니다. 이렇게 하면 입력 데이터가 SQL Injection 공격에 사용되지 않도록 보호됩니다. 파라미터화된 쿼리를 사용하면 데이터베이스 시스템이 입력을 안전하게 처리하므로 SQL Injection 공격을 방지할 수 있습니다.
-데이터베이스 및 프로그래밍 언어에 따라 파라미터화된 쿼리를 작성하는 방법이 다를 수 있으므로 해당 언어 및 데이터베이스에 맞는 문서 및 가이드를 참고하여 구현하는 것이 중요합니다.</p>
+                  <p>
+                    이 코드에서는 ?를 사용하여 입력 값을 파라미터화된 쿼리에
+                    전달합니다. 이렇게 하면 입력 데이터가 SQL Injection 공격에
+                    사용되지 않도록 보호됩니다. 파라미터화된 쿼리를 사용하면
+                    데이터베이스 시스템이 입력을 안전하게 처리하므로 SQL
+                    Injection 공격을 방지할 수 있습니다. 데이터베이스 및
+                    프로그래밍 언어에 따라 파라미터화된 쿼리를 작성하는 방법이
+                    다를 수 있으므로 해당 언어 및 데이터베이스에 맞는 문서 및
+                    가이드를 참고하여 구현하는 것이 중요합니다.
+                  </p>
                 </div>
               </div>
             </div>
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -14,33 +14,35 @@ export default function ResultPage({ location }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true); // 로딩 상태 관리
 
-    useEffect(() => {
-        putSpringData();
-        fetchData();
-    },[])
+  useEffect(() => {
+    putSpringData();
+    fetchData();
+  }, []);
 
-    async function putSpringData() {
-        await axios
-        .get("http://localhost:8081/analysis/result")
-        .then((res)=>{
-            console.log(res.data); 
-            setData(res.data);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    };
-    const fetchData = async () => {
-      try {
-          // mock API로부터 데이터 가져오기
-          const response = await axios.get(process.env.PUBLIC_URL + '/data/mockdata.json');
-          // 상태 업데이트
-          setData(response.data);
-      } catch (error) {
-          console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false); // 데이터 요청 완료(성공 또는 실패) 후 로딩 상태를 false로 설정
-      }
+  async function putSpringData() {
+    await axios
+      .get("http://localhost:8081/analysis/result")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  const fetchData = async () => {
+    try {
+      // mock API로부터 데이터 가져오기
+      const response = await axios.get(
+        process.env.PUBLIC_URL + "/data/mockdata.json"
+      );
+      // 상태 업데이트
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false); // 데이터 요청 완료(성공 또는 실패) 후 로딩 상태를 false로 설정
+    }
   };
   return (
     <>
@@ -49,12 +51,16 @@ export default function ResultPage({ location }) {
           <h2 className="font-bold text-4xl text-Result">Problem Chart</h2>
           <ul className="flex justify-center">
             <li className="bg-gray py-6 rounded-3xl ml-10">
-              <h2 className="text-Result text-2xl text-left mb-3">위험도 차트 risk chart</h2>
+              <h2 className="text-Result text-2xl text-left mb-3">
+                위험도 차트 risk chart
+              </h2>
               {/* 막대 차트 부분 */}
               {loading ? "Loading..." : <Barchart data={data} />}
             </li>
             <li className="bg-gray text-center py-6 rounded-3xl ml-32">
-              <h2 className="text-Result text-2xl text-left mb-3">취약점 차트 weakness chart</h2>
+              <h2 className="text-Result text-2xl text-left mb-3">
+                취약점 차트 weakness chart
+              </h2>
               {/* 파이 차트 부분 */}
               {loading ? `` : <Piechart data={data} />}
             </li>
@@ -63,22 +69,48 @@ export default function ResultPage({ location }) {
         <div className="m-4">
           <h2 className="font-bold text-4xl text-Result">Solution</h2>
           <div className="bg-gray text-center p-6 rounded-3xl m-3">
-          <table className="min-w-full divide-y divide-gray-200">
+            <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Number of Found</th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Risk</th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Category
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Number of Found
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Risk
+                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                {data ? data.map((datas)=>(
-                    <tr key={datas.category}>
-                        <td className="px-6 py-4 whitespace-normal"> {datas.category}</td>
-                        <td className="px-6 py-4 whitespace-normal"> {datas.payload}</td>
-                        <td className="px-6 py-4 whitespace-normal"> {datas.risk}</td>
-                    </tr>
-                )) : ''}
+                {data
+                  ? data.map((datas) => (
+                      <tr key={datas.category}>
+                        <td className="px-6 py-4 whitespace-normal">
+                          {" "}
+                          {datas.category}
+                        </td>
+                        <td className="px-6 py-4 whitespace-normal">
+                          {" "}
+                          {datas.payload}
+                        </td>
+                        <td className="px-6 py-4 whitespace-normal">
+                          {" "}
+                          {datas.risk}
+                        </td>
+                      </tr>
+                    ))
+                  : ""}
               </tbody>
             </table>
           </div>
@@ -86,18 +118,27 @@ export default function ResultPage({ location }) {
         <div className="m-4">
           <h2 className="font-bold text-4xl text-Result">Diagnosis</h2>
           <div className="py-6">
-            <h2 className="text-Result text-2xl text-left mb-3">진단 세부 사항</h2>
-            <Modal data={data}/>
+            <h2 className="text-Result text-2xl text-left mb-3">
+              진단 세부 사항
+            </h2>
+            <Modal data={data} />
           </div>
         </div>
         <div className="p-14 text-center ">
-            <Upbutton/>
+          <Upbutton />
         </div>
         <div className="bg-yourcodex bg-cover rounded-xl text-center p-10">
-          <h2 className="text-3xl text-white drop-shadow-text font-bold ">YourCode-X를 이용해주셔서 감사합니다.</h2>
-          <p className="text-white my-8">다른 웹 페에지에서도 취약점을 찾아보고 싶다면</p>
+          <h2 className="text-3xl text-white drop-shadow-text font-bold ">
+            YourCode-X를 이용해주셔서 감사합니다.
+          </h2>
+          <p className="text-white my-8">
+            다른 웹 페에지에서도 취약점을 찾아보고 싶다면
+          </p>
           <button className="px-16 py-4 bg-blue-600 rounded-xl my-4">
-            <Link to="/" className="text-white text-xl drop-shadow-text"> 첫페이지로 돌아가기</Link>
+            <Link to="/" className="text-white text-xl drop-shadow-text">
+              {" "}
+              첫페이지로 돌아가기
+            </Link>
           </button>
         </div>
       </div>
