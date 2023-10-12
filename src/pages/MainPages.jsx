d17ddf5cc8e76dadcf2e0449b4a4700bdfdc666d
ClownNero <ClownNero@gmail.com>
2023-10-12 15:28:21 +0900

    Merger TY

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+
 import { BsSearch } from "react-icons/bs";
+
 import { useNavigate } from "react-router-dom";
 import Introduce from "../components/Introduce";
 import Why from "../components/Why";
 import Provide from "../components/Provide";
+import Upbutton from "../components/ui/Upbutton";
 
 export default function MainPages(props) {
   const [url, setUrl] = useState("");
@@ -12,15 +15,29 @@ export default function MainPages(props) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(url);
+
     try {
-      // 서버에 POST 요청을 보내 URL 분석 요청
-      const response = await axios.post("/analysis/result", { url });
-      // 결과 페이지로 이동하면서 결과 데이터 전달
-      navigate("/analysis/result", { state: { result: response.data } });
+      const response = await fetch("http://localhost:5000/gomain", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ processedData: url }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Optional - if you expect a JSON response, you can parse the response:
+      const data = await response.json();
+
+      console.log(data);
     } catch (error) {
       navigate("/analysis/result");
-      console.error("Error during analysis: ", error);
+      console.error("Error during analysis:", error);
     }
+
     setUrl("");
   };
 
