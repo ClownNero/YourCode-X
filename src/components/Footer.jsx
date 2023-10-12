d902b7d58b1233af8215ac738ff137e3d368c3c1
ClownNero <ClownNero@gmail.com>
2023-10-12 16:04:44 +0900

    Modify Files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { BsSearch, BsYoutube } from "react-icons/bs";
+// import { Link, useNavigate, useParams } from "react-router-dom";
+// import { BsSearch, BsYoutube } from "react-icons/bs";
 
 export default function Footer(props) {
   return (
-    <>{/*
+    <>
+      {/*
       <footer className="p-8 text-2xl shadow-up">
         <ul className="w-full flex flex-row justify-between text-center text-sm">
           <div className="flex">
@@ -24,13 +25,13 @@ export default function Footer(props) {
         </ul>
       </footer>
   */}
-      <footer className="p-8 text-2xl shadow-up ">
-              <ul className="w-full h-[80px] flex flex-row justify-center items-center text-center text-sm">
-              <li className="mx-2 text-lg">
-                Copyright © {new Date().getFullYear()} YourCode-X cope.
-              </li>
-            </ul>
-          </footer>
+      <footer className="flex w-full p-8 text-2xl shadow-up ">
+        <ul className="w-full h-[80px] flex flex-row justify-center items-center text-center text-sm">
+          <li className="mx-2 text-lg">
+            Copyright © {new Date().getFullYear()} YourCode-X cope.
+          </li>
+        </ul>
+      </footer>
     </>
   );
 }
